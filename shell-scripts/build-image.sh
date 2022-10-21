3e3494a9778a452884e57e861c9ab3b076389e50
Christopher Sherman <chris@shermandigital.com>
2022-10-21 11:59:56 -0400

    Adjust build script

--- shell-scripts/build-image.sh
+++ shell-scripts/build-image.sh
@@ -1,1 +1,9 @@
-docker build -t aws-lambda-curl-url:latest ../ --progress=tty
+AWS_ACCOUNT_ID=$(aws sts get-caller-identity --query Account --output text)
+AWS_DEFAULT_REGION=$(aws configure get region)
+IMAGE_REPO_NAME=aws-lambda-curl-url
+IMAGE_TAG=latest
+
+aws ecr get-login-password --region $AWS_DEFAULT_REGION | docker login --username AWS --password-stdin $AWS_ACCOUNT_ID.dkr.ecr.$AWS_DEFAULT_REGION.amazonaws.com
+
+docker build -t ${IMAGE_REPO_NAME}:${IMAGE_TAG} ../ --progress=tty
+docker tag ${IMAGE_REPO_NAME}:${IMAGE_TAG} ${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_DEFAULT_REGION}.amazonaws.com/${IMAGE_REPO_NAME}:${IMAGE_TAG}
